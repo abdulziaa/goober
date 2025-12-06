49e52e6124929f55df3ffa950496657a51d694a9
Abdul Zia <52299063+abdulziaa@users.noreply.github.com>
2025-12-06 03:43:07 -0700

    Initial GOOBER library -- requires testing and documentation

---
+++ goober.c
@@ -0,0 +1,115 @@
+/**
+* @file goober.c
+* @author Abdul Zia (abdul@abdulzia.com)
+* @brief General Operations Over Basic Embedded Radio Packet Serializer/Deserializer
+* @version 1.0.0
+* @date 2025-12-06
+* 
+* @copyright Copyright (c) 2025 Abdul Zia. All rights reserved.
+* 
+*/
+
+#include "goober.h"
+
+/**
+* @brief Deserialize a buffer of bytes into a GOOBER packet header and payload.
+* 
+* @param incoming_buffer Pointer to the incoming buffer of bytes to be deserialized.
+* @param incoming_buffer_size Size of the incoming buffer in bytes.
+* @param header Pointer to the header struct to be populated.
+* @param payload Pointer to the payload buffer to be populated.
+* @param payload_size Pointer to the size of the payload in bytes.
+* @return int Error code. 0 if successful, otherwise a negative error code.
+*/
+int goober_deserialize(uint8_t *incoming_buffer, size_t incoming_buffer_size, goober_header_t *header, uint8_t *payload, size_t payload_buffer_size, size_t *payload_size)
+{
+    if (incoming_buffer == NULL || incoming_buffer_size == 0 || header == NULL || payload == NULL || payload_size == NULL) { // is the input invalid?
+        return GOOBER_ERROR_INVALID_INPUT;
+    }
+
+    if (incoming_buffer_size > (GOOBER_HEADER_SIZE + MAX_PAYLOAD_LENGTH)) { // is the packet too large to be a valid GOOBER packet?
+        return GOOBER_ERROR_PACKET_TOO_LARGE;
+    }
+
+    if (incoming_buffer_size < GOOBER_HEADER_SIZE) { // is the packet too small to be a valid GOOBER packet?
+        return GOOBER_ERROR_PACKET_TOO_SMALL;
+    }
+
+    header->dev_id = incoming_buffer[0];
+    header->dev_mode = incoming_buffer[1];
+    header->seq_id = incoming_buffer[2];
+    header->msg_cls = incoming_buffer[3];
+    header->payload_length = incoming_buffer[4];
+
+    if (header->payload_length > MAX_PAYLOAD_LENGTH) { // is the payload length too large?
+        return GOOBER_ERROR_INVALID_PAYLOAD_LENGTH;
+    }
+
+    if (incoming_buffer_size < (GOOBER_HEADER_SIZE + header->payload_length)) { // is the packet smaller than expected?
+        return GOOBER_ERROR_PAYLOAD_LENGTH_MISMATCH_TOO_SMALL;
+    }
+
+    if (incoming_buffer_size > (GOOBER_HEADER_SIZE + header->payload_length)) { // is the packet larger than expected?
+        return GOOBER_ERROR_PAYLOAD_LENGTH_MISMATCH_TOO_LARGE;
+    }
+
+    if (header->payload_length == 0) { // is the payload length 0?
+        *payload_size = 0;
+        return 0; // zero payload is valid, just don't copy anything
+    }
+
+    if (payload_buffer_size < header->payload_length) { // is the payload buffer too small to contain the payload?
+        return GOOBER_ERROR_PAYLOAD_BUFFER_TOO_SMALL;
+    }
+
+    memcpy(payload, incoming_buffer + GOOBER_HEADER_SIZE, header->payload_length); // copy the payload into the payload buffer
+    *payload_size = header->payload_length;
+
+    return 0; // success!
+}
+
+/**
+ * @brief 
+ * 
+ * @param header 
+ * @param payload_buffer 
+ * @param payload_size 
+ * @param serialized_buffer 
+ * @param serialized_buffer_size 
+ * @param serialized_packet_size 
+ * @return int 
+ */
+int goober_serialize(goober_header_t header, uint8_t *payload_buffer, size_t payload_size, uint8_t *serialized_buffer, size_t serialized_buffer_size, size_t *serialized_packet_size)
+{
+    if (payload_buffer == NULL || serialized_buffer == NULL || serialized_packet_size == NULL) { // is the input invalid?
+        return GOOBER_ERROR_INVALID_INPUT;
+    }
+
+    if (payload_size > MAX_PAYLOAD_LENGTH) { // is the payload too large to be a valid GOOBER packet?
+        return GOOBER_ERROR_INVALID_PAYLOAD_LENGTH;
+    }
+
+    if (serialized_buffer_size < GOOBER_HEADER_SIZE) { // is the allocated serialized buffer too small to contain the header?
+        return GOOBER_ERROR_SERIALIZED_BUFFER_INVALID;
+    }
+
+    if (serialized_buffer_size < GOOBER_HEADER_SIZE + payload_size) { // is the allocated serialized buffer too small to contain the packet?
+        return GOOBER_ERROR_SERIALIZED_BUFFER_TOO_SMALL;
+    }
+
+    serialized_buffer[0] = header.dev_id;
+    serialized_buffer[1] = header.dev_mode;
+    serialized_buffer[2] = header.seq_id;
+    serialized_buffer[3] = header.msg_cls;
+    serialized_buffer[4] = payload_size;
+
+    if (payload_size == 0) { // is the payload length 0?
+        *serialized_packet_size = GOOBER_HEADER_SIZE;
+        return 0; // zero payload is valid, just don't copy anything
+    }
+
+    memcpy(serialized_buffer + GOOBER_HEADER_SIZE, payload_buffer, payload_size); // copy the payload into the serialized buffer
+    *serialized_packet_size = GOOBER_HEADER_SIZE + payload_size;
+
+    return 0; // success!
+}
---
+++ goober.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAX_PAYLOAD_LENGTH 250
+#define GOOBER_HEADER_SIZE 5
+
+enum goober_error_t {
+    GOOBER_ERROR_PACKET_TOO_LARGE = -1,
+    GOOBER_ERROR_PACKET_TOO_SMALL = -2,
+    GOOBER_ERROR_INVALID_PAYLOAD_LENGTH = -3,
+    GOOBER_ERROR_PAYLOAD_LENGTH_MISMATCH_TOO_SMALL = -4,
+    GOOBER_ERROR_PAYLOAD_LENGTH_MISMATCH_TOO_LARGE = -5,
+    GOOBER_ERROR_SERIALIZED_BUFFER_INVALID = -6,
+    GOOBER_ERROR_SERIALIZED_BUFFER_TOO_SMALL = -7,
+    GOOBER_ERROR_PAYLOAD_BUFFER_TOO_SMALL = -8,
+    GOOBER_ERROR_HEADER_PAYLOAD_LENGTH_MISMATCH = -9,
+    GOOBER_ERROR_INVALID_INPUT = -99,
+};
+
+typedef struct goober_header {
+    uint8_t dev_id;
+    uint8_t dev_mode;
+    uint8_t seq_id;
+    uint8_t msg_cls;
+    uint8_t payload_length;
+} goober_header_t;
+
+int goober_deserialize(uint8_t *incoming_buffer, size_t incoming_buffer_size, goober_header_t *header, uint8_t *payload, size_t payload_buffer_size, size_t *payload_size);
+int goober_serialize(goober_header_t header, uint8_t *payload_buffer, size_t payload_size, uint8_t *serialized_buffer, size_t serialized_buffer_size, size_t *serialized_packet_size);
